86b4aa78c01fed5613512d0635147c3bec6265ee
bhardwajritika <39398560+bhardwajritika@users.noreply.github.com>
2022-03-13 14:11:31 +0530

    Add files via upload

---
+++ March/week1/Assignment(20-24)_Week-2/Assignment21/Add2DMatrices.java
@@ -0,0 +1,56 @@
+package assignment21;
+import java.util.*;
+
+public class Add2DMatrices {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter no. of rows of the matrices of addition :: ");
+		int row= scan.nextInt();
+		
+		System.out.println("Enter no. of columns of the matrices of addition ::");
+		int cols = scan.nextInt();
+		System.out.println("Enter first matrix::  ");
+		
+		int[][] arr1= new int[row][cols];
+		
+		for(int i=0; i<row;i++) {
+			for(int j=0;j<cols;j++) {
+				arr1[i][j]= scan.nextInt();
+				
+			}
+		}
+		
+System.out.println("Enter second matrix::  ");
+		
+		int[][] arr2= new int[row][cols];
+		
+		for(int i=0; i<row;i++) {
+			for(int j=0;j<cols;j++) {
+				arr2[i][j]= scan.nextInt();
+				
+			}
+		}
+		int[][] arr3 = new int[row][cols];
+		
+		for(int i=0; i<row;i++) {
+			for(int j=0;j<cols;j++) {
+				arr3[i][j] =arr1[i][j] + arr2[i][j];
+				
+			}
+		}
+		
+		for(int i=0; i<row;i++) {
+			for(int j=0;j<cols;j++) {
+				System.out.print(arr3[i][j] + " ");
+				
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
