4b331f93bf7171b7b3a088cb0f447dc2841f6557
bhardwajritika <39398560+bhardwajritika@users.noreply.github.com>
2022-03-20 17:20:21 +0530

    Add files via upload

---
+++ March/week1/Assignment(25-29)/Assignment26/Employee.java
@@ -0,0 +1,40 @@
+package assignment26;
+
+class Employeess{
+    int id;
+    String name;
+    String department;
+    String worklocation = "Banglore";
+    
+     public int getId() { return id; }
+ 
+    public void setId(int id) { this.id = id; }
+    
+     public String getname() { return name; }
+ 
+    public void setname(String name) { this.name = name; }
+    
+     public String getdepartment() { return department; }
+ 
+    public void setdepartment(String department) { this.department = department; }
+    
+    public String getworklocation() { return worklocation; }
+    
+     
+    
+}
+
+public class Employee {
+    public static void main(String args[]) {
+        Employeess E1 = new Employeess();
+      E1.setId(111);
+      E1.setname("Ritika");
+      E1.setdepartment("Training");
+        
+        System.out.println(E1.getname());
+        System.out.println(E1.getId());
+        System.out.println(E1.getdepartment());
+        System.out.println(E1.getworklocation());
+
+    }
+}
